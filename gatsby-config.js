4583133d979215aef2e4cf10a08142711dd28ddd
ss4223 <>
2020-08-14 18:24:59 +0900

    remove gatsby-source-filesystem setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,14 +49,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      },
-    },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
